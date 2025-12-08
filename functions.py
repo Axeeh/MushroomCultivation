bf251a5c59f27da9fb4e81b0f6abedde67388663
ManuCa <manucatto14@gmail.com>
2025-12-08 16:43:20 +0100

    added metroplis and rght gridding

--- functions.py
+++ functions.py
@@ -22,9 +22,17 @@ def neg_log_lik(theta, x, y, n):
 def neg_lik(theta, x, y, n):
     return -lik(theta, x, y, n)
 
-def log_prior(theta, sigma_prior=10):
+# def log_prior(theta, sigma_prior=10):
+#     alpha, beta = theta
+#     return - (alpha**2 + beta**2) / (2 * sigma_prior**2)
+
+def log_prior(theta):
     alpha, beta = theta
-    return - (alpha**2 + beta**2) / (2 * sigma_prior**2)
+        
+    lp_alpha = - (alpha**2) / (2 * 2**2)  # sigma=2
+    lp_beta =  - (beta**2)  / (2 * 1**2)  # sigma=1
+    
+    return lp_alpha + lp_beta
 
 def log_posterior(theta, x, y, n):
     return log_lik(theta, x,y,n) + log_prior(theta)
